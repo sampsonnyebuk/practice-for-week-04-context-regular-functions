7e1b507cdf3bc35ee5c55860637eb6dc51d7f0e9
ukemeedidiong <ukemeedidiong226@gmail.com>
2024-10-25 13:59:38 +0100

    my first commit

--- context.js
+++ context.js
@@ -15,23 +15,23 @@ class Fish {
 const nemo = new Fish('Nemo');
 
 /********************************* Scenario 1 *********************************/
-// eat(); // ?
+ eat(); //undefined eats fish food ?
 
 
 /********************************* Scenario 2 *********************************/
-// nemo.eat = eat;
-// nemo.eat(); // ?
+nemo.eat = eat;
+nemo.eat(); // Nemo eats fish food ?
 
 
 /********************************* Scenario 3 *********************************/
-// nemo.eat = eat;
-// eat(); // ?
+nemo.eat = eat;
+eat(); //undefined eats fish food ?
 
 
 /********************************* Scenario 4 *********************************/
-// nemo.swim(); // ?
+nemo.swim(); // Nemo swimming in the water ?
 
 
 /********************************* Scenario 5 *********************************/
-// const swim = nemo.swim;
-// swim(); // ?
+const swim = nemo.swim;
+swim(); //error ?
